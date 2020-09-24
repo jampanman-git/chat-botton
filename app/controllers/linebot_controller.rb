8b8f05fee3e61a46a9bf559691d11ef46144a436
jampanman-git <kosukengineer@gmail.com>
2020-09-25 01:02:53 +0900

    コントローラーの修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -9,7 +9,7 @@ class LinebotController < ApplicationController
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
- 
+
   def callback
     body = request.body.read
 
@@ -63,5 +63,4 @@ class LinebotController < ApplicationController
       }
     }
   end
-
 end
